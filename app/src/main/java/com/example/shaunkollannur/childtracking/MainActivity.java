90d045cf4fcdbf9b57f18dc3ce8ece44d35b880c
shauniop <shaunkollannur@Shauns-MacBook-Air-2.local>
2017-02-03 20:21:11 +0530

    Some changes with better notification

--- app/src/main/java/com/example/shaunkollannur/childtracking/MainActivity.java
+++ app/src/main/java/com/example/shaunkollannur/childtracking/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.shaunkollannur.childtracking;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
@@ -58,11 +61,19 @@ public class MainActivity extends AppCompatActivity {
         pusher.connect();
     }
     private void addNotification() {
+        Bitmap bmp= BitmapFactory.decodeResource(this.getResources(),R.drawable.icon);
+        NotificationCompat.BigPictureStyle style=new NotificationCompat.BigPictureStyle();
+        style.setBigContentTitle("Child Tracker");
+        style.setBigContentTitle(s);
+        style.bigPicture(bmp);
         NotificationCompat.Builder builder =
                 (NotificationCompat.Builder) new NotificationCompat.Builder(this)
                         .setSmallIcon(android.R.drawable.ic_menu_save)
                         .setContentTitle("Notifications Example")
-                        .setContentText(s);
+                        .setContentText("Check the app for further details")
+                        .setAutoCancel(true)
+                        .setStyle(style);
+
 
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
@@ -72,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
         // Add as notification
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         manager.notify(0, builder.build());
+
     }
 
 }
