64d55a8766e39f6e3cb67b6a01c99a64b05fcffd
shauniop <shaunkollannur@Shauns-MacBook-Air-2.local>
2017-02-05 00:09:42 +0530

    more efficient2

--- app/src/main/java/com/example/shaunkollannur/childtracking/MainActivity.java
+++ app/src/main/java/com/example/shaunkollannur/childtracking/MainActivity.java
@@ -1,26 +1,9 @@
 package com.example.shaunkollannur.childtracking;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.NotificationCompat;
 import android.view.View;
-
-import com.pusher.client.Pusher;
-import com.pusher.client.PusherOptions;
-import com.pusher.client.channel.Channel;
-import com.pusher.client.channel.SubscriptionEventListener;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     String str;
