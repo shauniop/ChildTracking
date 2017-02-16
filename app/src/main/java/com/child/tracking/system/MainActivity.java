fbef44a2d7ec7b12143b5b57c80cccfa02e6cacb
drishit96 <drishitmitra@gmail.com>
2017-02-16 23:39:06 +0530

    Added Google Maps to show location of child as sent from server

--- app/src/main/java/com/child/tracking/system/MainActivity.java
+++ app/src/main/java/com/child/tracking/system/MainActivity.java
@@ -1,16 +1,75 @@
 package com.child.tracking.system;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends AppCompatActivity {
+import static com.child.tracking.system.R.id.map;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private static final int MY_LOCATION_REQUEST_CODE = 21;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseMessaging.getInstance().subscribeToTopic("450");
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+            if (permissions.length == 1 &&
+                    permissions[0] == android.Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d("MapActivity", "onMapReady: ");
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (getIntent().getExtras() != null) {
+                String lat = getIntent().getExtras().getString("latitude");
+                String lng = getIntent().getExtras().getString("longitude");
+                if (lat != null && lng != null) {
+                    Double latitude = Double.parseDouble(lat);
+                    Double longitude = Double.parseDouble(lng);
+                    googleMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(latitude, longitude))
+                            .title("Marker"));
+
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(new LatLng(latitude, longitude))      // Sets the center of the map to location user
+                            .zoom(15)                   // Sets the zoom
+                            .bearing(0)                // Sets the orientation of the camera to east
+                            .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                            .build();                   // Creates a CameraPosition from the builder
+                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                }
+            }
+            else {
+                googleMap.setMyLocationEnabled(true);
+            }
+        }
     }
 }
